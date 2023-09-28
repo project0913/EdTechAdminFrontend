f54dea3636de4ef93c04be36492ef7a579f07a3a
Amanuel Shiferaw <123aman64@gmail.com>
2023-09-28 12:48:57 +0300

    fix base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:3000"; // "https://coydoe.onrender.com",
+export const baseURL = "https://coydoe.onrender.com"; // "http://localhost:3000"; // ,
 
 console.log("axios token loaded");
 
