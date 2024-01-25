8d55235b93374c430a8ece51fc27eb8e770da543
Amanuel Shiferaw <123aman64@gmail.com>
2024-01-25 23:58:20 +0300

    feat: add get admin challenge

--- src/DataService/adminChallenge.service.ts
+++ src/DataService/adminChallenge.service.ts
@@ -7,3 +7,33 @@ export const createAdminChallenge = async (adminChallengeDto: any) => {
   const data = result.data;
   return data;
 };
+
+export const getAdminChallenge = async (query: {
+  startDate?: string;
+  endDate?: string;
+  level?: number;
+  page?: number;
+  size?: number;
+  isActive?: boolean;
+}) => {
+  let queryBuilder = "?";
+  const page = `${query.page ? query.page : 1}`;
+  const size = `${query.size ? query.size : 10}`;
+  const queryArray = [];
+
+  if (query.endDate && query.startDate) {
+    queryBuilder += `startDate=${query.startDate}&endDate=${query.endDate}&`;
+  }
+
+  if (query.level) {
+    queryBuilder += `level=${query.level}&`;
+  }
+
+  if (query.isActive !== null && query.isActive !== undefined) {
+    queryBuilder += `isActive=${query.isActive}`;
+  }
+
+  const result = await axios.get("/admin-challenge" + queryBuilder);
+  const data = result.data;
+  return data;
+};
